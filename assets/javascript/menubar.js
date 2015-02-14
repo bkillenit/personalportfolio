906f16ec4e843481d99a8f58799fe268223d32c8
bkillenit <blaine.j.killen@gmail.com>
2015-02-14 01:24:33 -0600

    menu bar html now inserted modularly, correctly following DRY principles

---
+++ assets/javascript/menubar.js
@@ -0,0 +1,12 @@
+var menuBarHTML = ' \
+  <nav class="ui menu fixed"> \
+    <a href="./index.html" class="active item">Home</a> \
+    <div class="right menu"> \
+      <a href="./portfolio.html" class="item">Portfolio</a> \
+      <a href="./about.html" class="item">About</a> \
+      <a href="./contact.html" class="item">Contact</a> \
+  	</div> \
+  </nav> \
+';
+
+document.write(menuBarHTML);
