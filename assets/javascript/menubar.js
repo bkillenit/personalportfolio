e400c73f0b51efd4a67eacea7364bda45bfb691c
bkillenit <blaine.j.killen@gmail.com>
2015-02-17 22:03:34 -0600

    fixed html div structure and DOM element classes to better fit the requirements of Semantic UI, added nav bar functionality that correctly highlights the page on the nav bar that the user is on

--- assets/javascript/menubar.js
+++ assets/javascript/menubar.js
@@ -1,12 +1,38 @@
 var menuBarHTML = ' \
   <nav class="ui menu fixed"> \
-    <a href="./index.html" class="active item">Home</a> \
+    <a href="./index.html" id="index" class="item">Home</a> \
     <div class="right menu"> \
-      <a href="./portfolio.html" class="item">Portfolio</a> \
-      <a href="./about.html" class="item">About</a> \
-      <a href="./contact.html" class="item">Contact</a> \
+      <a href="./about.html" id="about" class="item">About</a> \
+      <a href="./portfolio.html" id="portfolio" class="item">Portfolio</a> \
+      <a href="./contact.html" id="contact" class="item">Contact</a> \
   	</div> \
   </nav> \
 ';
 
 document.write(menuBarHTML);
+
+// the block of code below checks which page the user is on and adds the active class to the corresponding nav bar
+// element to represent where they are within the website
+curPageURL = window.location.pathname;
+
+function contains(string, searchValue) {
+	if(string.indexOf(searchValue) > -1 ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+activePage = "";
+navBarPagesArray = ["index","about", "portfolio", "contact"];
+
+for(i = 0; i < navBarPagesArray.length; i++) {
+	if(contains(curPageURL, navBarPagesArray[i])) {
+		activePage = navBarPagesArray[i];
+		break;
+	}
+}
+
+//used vanilla javascript to edit the DOM element instead of jQuery to avoid importing the jQuery library
+var activeNavElement = document.getElementById(activePage);
+activeNavElement.className = activeNavElement.className + " active";
