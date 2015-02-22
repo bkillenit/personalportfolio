6a53a7dc41d8eee2bb2ff108938550fb6df4cecc
bkillenit <blaine.j.killen@gmail.com>
2015-02-21 18:33:05 -0600

    added experience html page and added content to the contact page

--- assets/javascript/menubar.js
+++ assets/javascript/menubar.js
@@ -3,6 +3,7 @@ var menuBarHTML = ' \
     <a href="./index.html" id="index" class="item">Home</a> \
     <div class="right menu"> \
       <a href="./about.html" id="about" class="item">About</a> \
+      <a href="./experience.html" id="experience" class="item">Experience</a> \
       <a href="./portfolio.html" id="portfolio" class="item">Portfolio</a> \
       <a href="./contact.html" id="contact" class="item">Contact</a> \
   	</div> \
@@ -14,6 +15,8 @@ document.write(menuBarHTML);
 // the block of code below checks which page the user is on and adds the active class to the corresponding nav bar
 // element to represent where they are within the website
 curPageURL = window.location.pathname;
+activePage = "";
+navBarPagesArray = ["index","about", "experience","portfolio", "contact"];
 
 function contains(string, searchValue) {
 	if(string.indexOf(searchValue) > -1 ) {
@@ -22,9 +25,6 @@ function contains(string, searchValue) {
 		return false;
 	}
 }
-
-activePage = "";
-navBarPagesArray = ["index","about", "portfolio", "contact"];
 
 for(i = 0; i < navBarPagesArray.length; i++) {
 	if(contains(curPageURL, navBarPagesArray[i])) {
