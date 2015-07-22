fb5f4b1c5c9b3ea4b641b6cef2c76a79e9b41e4b
bkillenit <blaine.j.killen@gmail.com>
2015-07-21 21:59:00 -0500

    added logos for technologies used in projects and changed portfolio page to projects page

---
+++ assets/javascript/languageLogos.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.python').attr('src', 'assets/images/logos/python.png');
+
+	$('.ableton').attr('src', 'assets/images/logos/ableton.png');
+
+	$('.kinect').attr('src', 'assets/images/logos/kinect.png');
+
+	$('.mean').attr('src', 'assets/images/logos/mean.png');
+
+	$('.django').attr('src', 'assets/images/logos/django.png');
+
+	$('.stanford-nlp').attr('src', 'assets/images/logos/stanford-nlp.png');
+
+	$('.rasbian').attr('src', 'assets/images/logos/rasbian.png');
+
+	$('.ruby-on-rails').attr('src', 'assets/images/logos/ruby-on-rails.png');
+
+	$('.arduino').attr('src', 'assets/images/logos/arduino.png');
+});
--- assets/javascript/menubar.js
+++ assets/javascript/menubar.js
@@ -1,36 +1,39 @@
-var menuBarHTML = ' \
-  <nav class="ui menu fixed"> \
-    <a href="./" id="index" class="item">Home</a> \
-    <div class="right menu"> \
-      <a href="./about.html" id="about" class="item">About</a> \
-      <a href="./experience.html" id="experience" class="item">Experience</a> \
-      <a href="./portfolio.html" id="portfolio" class="item">Portfolio</a> \
-      <a href="./contact.html" id="contact" class="item">Contact</a> \
-  	</div> \
-  </nav> \
-';
+$(document).ready(function(){
+	var menuBarHTML = ' \
+		<div class="ui fixed menu"> \
+			<a href="./" id="index" class="item">Home</a> \
+			<div id="contentLinks" class="menu"> \
+				<a href="./about.html" id="about" class="item">About</a> \
+				<a href="./experience.html" id="experience" class="item">Experience</a> \
+				<a href="./projects.html" id="projects" class="item">Projects</a> \
+				<a href="./contact.html" id="contact" class="item">Contact</a> \
+			</div> \
+		</div> \
+	';
 
-document.write(menuBarHTML);
+	// alert( $( document ).width());
+	$('body').prepend($.parseHTML(menuBarHTML));
 
-// the block of code below checks which page the user is on and adds the active class to the corresponding nav bar
-// element to represent where they are within the website
-curPageURL = window.location.pathname;
-activePage = "";
-navBarPagesArray = ["index","about", "experience","portfolio", "contact"];
+	// the block of code below checks which page the user is on and adds the active class to the corresponding nav bar
+	// element to represent where they are within the website
+	curPageURL = window.location.pathname;
+	activePage = "";
+	navBarPagesArray = ["index","about", "experience","projects", "contact"];
 
-function contains(string, searchValue) {
-	return string.indexOf(searchValue) > -1;
-}
-
-for(i = 0; i < navBarPagesArray.length; i++) {
-	if(contains(curPageURL, navBarPagesArray[i])) {
-		activePage = navBarPagesArray[i];
-		break;
+	function contains(string, searchValue) {
+		return string.indexOf(searchValue) > -1;
 	}
 
-	// sets the active page to index if none of the nav bar titles are found, ie. on the index page
-	activePage = "index";
-}
+	for(i = 0; i < navBarPagesArray.length; i++) {
+		if(contains(curPageURL, navBarPagesArray[i])) {
+			activePage = navBarPagesArray[i];
+			break;
+		}
 
-// jQuery DOM element selection based on active page, active class added to the selected element
-$('#'+activePage).addClass("active");
+		// sets the active page to index if none of the nav bar titles are found, ie. on the index page
+		activePage = "index";
+	}
+
+	// jQuery DOM element selection based on active page, active class added to the selected element
+	$('#'+activePage).addClass("active");
+});
